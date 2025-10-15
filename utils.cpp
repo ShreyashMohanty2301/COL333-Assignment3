0533df63febe92d0a28236433af7a8de6c222f0b
nimit435 <nimitjain435@gmail.com>
2025-10-15 23:38:43 +0530

    turn limit added

--- utils.cpp
+++ utils.cpp
@@ -1,4 +1,3 @@
-#include "utils.h"
 #include "encode.cpp"
 
 vector<int> get_ids(long long i , long long j , long long m , long long t) {
@@ -18,7 +17,7 @@ void oneEntry(long long n , long long m , long long t , vector<vector<long long>
                     res.push_back((i*m + j - 1)*(8LL*t + 2) + 8*k + l);
                 }
             }
-            encode(n , m ,res , clauses , 1);
+            encode(res , clauses , n, m, t, 1);
         }
     }
 }
@@ -32,7 +31,7 @@ void oneExit(long long n , long long m , long long t , vector<vector<long long>>
                     res.push_back((i*m + j - 1)*(8LL*t + 2) + 8*k + l);
                 }
             }
-            encode(n , m , res , clauses , 1);
+            encode(res , clauses , n ,m , t, 1);
         }
     }
 }
@@ -58,7 +57,7 @@ void flow(long long n , long long m , long long t , vector<vector<long long>>& c
                     res.push_back(-1LL*((i*m + j - 1)*(8LL*t + 2) + 8*k + l));
                     for(int e = 1 ; e <= 7 ; e++) {
                         if(e != 4 && e + l >= 4 && e + l < 8) {
-                            res.push_back((i*m + j - 1)*(8LL*t + 2) + 8*k + l + e)
+                            res.push_back((i*m + j - 1)*(8LL*t + 2) + 8*k + l + e);
                         } 
                     }
                 }
@@ -74,8 +73,8 @@ void strtPoint(long long n , long long m , long long t , vector<vector<long long
             for(int k = 1; k <= t; k++) {
                 for(int l = 0; l < 4; l++){
                     vector<long long> res;
-                    res.push_back(-1LL((i*m + j - 1)*(8LL*t + 2) + 8LL*t + 1));
-                    res.push_back(-1LL((i*m + j - 1)*(8LL*t + 2) + 8LL*k + l));
+                    res.push_back((-1LL)*((i*m + j - 1)*(8LL*t + 2) + 8LL*t + 1));
+                    res.push_back((-1LL)*((i*m + j - 1)*(8LL*t + 2) + 8LL*k + l));
                     clauses.push_back(res);
                 }
             }
@@ -93,5 +92,32 @@ void endPoint(long long n , long long m , long long t , vector<vector<long long>
             }
             clauses.push_back(res);
         }
+    }
+}
+
+void limit_on_turns(long long n , long long m , long long t , long long J , vector<vector<long long>>& clauses) {
+    auto get_id = [&](int i, int j, int k, int dir, bool entry){
+        return ((i*m)+(j-1))*(8*t +2)+ (8*k + (entry ? dir : dir + 4));
+    };
+    auto get_dir = [](int dir){
+        dir = dir%4;
+        if(dir ==0) return 4;
+        else return dir;
+    };
+    for(int k = 0; k<t; k++){
+        vector<vector<long long>> clauses;
+        for(int i=1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                vector<long long> temp;
+                for(int dir = 0; dir < 4; dir++){
+                    temp = {get_id(i,j,k,dir,true), get_id(i,j,k,get_dir(dir+1),false)};
+                    clauses.push_back(temp);
+                    temp = {get_id(i,j,k,dir,true), get_id(i,j,k,get_dir(dir+3),false)};
+                    clauses.push_back(temp);
+                }
+                
+            }
+        }
+        encode(clauses , clauses , n, m, t, J);
     }
 }
--- utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-#include <vector>
-using namespace std;
-
-void oneEntry(int n , int m , vector<vector<int>>& clauses);
-#endif
