63d04284713376e84dd9daae42baa44d58992332
ShreyashMohanty2301 <es1221797@iitd.ac.in>
2025-10-15 23:38:53 +0530

    Minor change

--- utils.cpp
+++ utils.cpp
@@ -1,21 +1,13 @@
 #include "utils.h"
 #include "encode.cpp"
 
-vector<int> get_ids(long long i , long long j , long long m , long long t) {
-    vector<int> res;
-    for(int k = 0 ; k < 8 ; k++) {
-        res.push_back(i*m + j + 8*t + k);
-    }
-    return res;
-}
-
 void oneEntry(long long n , long long m , long long t , vector<vector<long long>>& clauses) {
     for(long long i = 1; i <= n; i++) {
         for(long long j = 1; j <= m; j++) {
             vector<long long> res;
-            for(int k = 1; k <= t ; k++) {
-                for(int l = 0; l < 4; l++) {
-                    res.push_back((i*m + j - 1)*(8LL*t + 2) + 8*k + l);
+            for(int k = 0; k <= t - 1 ; k++) {
+                for(int l = 1; l <= 4; l++) {
+                    res.push_back((i*m + j - 1)*(8LL*t) + 8*k + l);
                 }
             }
             encode(n , m ,res , clauses , 1);
@@ -27,9 +19,9 @@ void oneExit(long long n , long long m , long long t , vector<vector<long long>>
     for(long long i = 1; i <= n; i++) {
         for(long long j = 1; j <= m; j++) {
             vector<long long> res;
-            for(int k = 1; k <= t ; k++) {
-                for(int l = 4; l < 8; l++) {
-                    res.push_back((i*m + j - 1)*(8LL*t + 2) + 8*k + l);
+            for(int k = 0; k <= t - 1 ; k++) {
+                for(int l = 5; l <= 8; l++) {
+                    res.push_back((i*m + j - 1)*(8LL*t) + 8*k + l);
                 }
             }
             encode(n , m , res , clauses , 1);
@@ -41,10 +33,10 @@ void flow(long long n , long long m , long long t , vector<vector<long long>>& c
     for(long long i = 1; i <= n; i++) {
         for(long long j = 1; j <= m; j++) {
             vector<long long> res;
-            for(int k = 1; k <= t ; k++) {
-                for(int l = 0; l < 4; l++) {
-                    res.push_back(-1LL*((i*m + j - 1)*(8LL*t + 2) + 8*k + l));
-                    res.push_back(-1LL*((i*m + j - 1)*(8LL*t + 2) + 8*k + l + 4));
+            for(int k = 0; k <= t - 1 ; k++) {
+                for(int l = 1; l <= 4; l++) {
+                    res.push_back(-1LL*((i*m + j - 1)*(8LL*t) + 8*k + l));
+                    res.push_back(-1LL*((i*m + j - 1)*(8LL*t) + 8*k + l + 4));
                 }
             }
             clauses.push_back(res);
@@ -53,12 +45,12 @@ void flow(long long n , long long m , long long t , vector<vector<long long>>& c
     for(long long i = 1; i <= n; i++) {
         for(long long j = 1; j <= m; j++) {
             vector<long long> res;
-            for(int k = 1; k <= t ; k++) {
-                for(int l = 0; l < 4; l++) {
-                    res.push_back(-1LL*((i*m + j - 1)*(8LL*t + 2) + 8*k + l));
+            for(int k = 0; k <= t - 1 ; k++) {
+                for(int l = 1; l <= 4; l++) {
+                    res.push_back(-1LL*((i*m + j - 1)*(8LL*t) + 8*k + l));
                     for(int e = 1 ; e <= 7 ; e++) {
                         if(e != 4 && e + l >= 4 && e + l < 8) {
-                            res.push_back((i*m + j - 1)*(8LL*t + 2) + 8*k + l + e)
+                            res.push_back((i*m + j - 1)*(8LL*t) + 8*k + l + e);
                         } 
                     }
                 }
@@ -68,30 +60,3 @@ void flow(long long n , long long m , long long t , vector<vector<long long>>& c
     }
 }
 
-void strtPoint(long long n , long long m , long long t , vector<vector<long long>>& clauses) {
-    for(long long i = 1; i <= n; i++) {
-        for(long long j = 1; j <= m; j++) {
-            for(int k = 1; k <= t; k++) {
-                for(int l = 0; l < 4; l++){
-                    vector<long long> res;
-                    res.push_back(-1LL((i*m + j - 1)*(8LL*t + 2) + 8LL*t + 1));
-                    res.push_back(-1LL((i*m + j - 1)*(8LL*t + 2) + 8LL*k + l));
-                    clauses.push_back(res);
-                }
-            }
-        }
-    }
-}
-
-void endPoint(long long n , long long m , long long t , vector<vector<long long>>& clauses) {
-    for(long long i = 1; i <= n; i++) {
-        for(long long j = 1; j <= m; j++) {
-            vector<long long> res;
-            res.push_back(-1LL*((m*i + j)*(8LL*t + 2) + 8LL*t + 2));
-            for(int k = 4*t; k < 8LL*t; k++) {
-                res.push_back((m*i + j)*(8LL*t + 2) + k);
-            }
-            clauses.push_back(res);
-        }
-    }
-}
